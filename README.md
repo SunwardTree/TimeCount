f505ac4293833aafe4648f5fa654af69a7054d6e
SunwardTree <sunwardtree@outlook.com>
2017-10-22 00:33:36 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,36 @@
+# CircleTimerView
+Circle timer on Android platform. <br />
+<img src="./images/sample.gif" width="200px" height="auto" />
+<img src="./images/sample2.gif" width="200px" height="auto" />
+
+## System Requirement
+Android v2.2+
+
+## TODO
+Developed by AndroidStudio
+
+## Usage
+```xml
+<com.jiahuan.circletimerview.CircleTimerView
+    android:id="@+id/ctv"
+    android:layout_width="300dp"
+    android:layout_height="300dp"
+    android:layout_centerInParent="true">
+</com.jiahuan.circletimerview.CircleTimerView>
+```
+Ensure the height equals to the width
+
+## Notice
+If you make the targetSdkVersion greater than or equal to 14, please add the following code in the end of the `initialize` method. This code is to solve the problem that no shadow in higher targetSdkVersion
+```java
+setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+```
+
+## License
+Copyright 2015 JiaHuan
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ sample/src/main/java/com/jiahuan/circletimerview/sample/MainActivity.java
@@ -0,0 +1,79 @@
+package com.jiahuan.circletimerview.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.jiahuan.circletimerview.CircleTimerView;
+
+
+public class MainActivity extends Activity implements CircleTimerView.CircleTimerListener
+{
+
+    CircleTimerView timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timer = (CircleTimerView) findViewById(R.id.ctv);
+        timer.setCircleTimerListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void start(View v)
+    {
+        timer.startTimer();
+    }
+
+    public void pause(View v)
+    {
+        timer.pauseTimer();
+    }
+
+    @Override
+    public void onTimerStop()
+    {
+        Toast.makeText(this, "onTimerStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onTimerStart()
+    {
+        Toast.makeText(this, "onTimerStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onTimerPause()
+    {
+        Toast.makeText(this, "onTimerPause", Toast.LENGTH_LONG).show();
+    }
+}
